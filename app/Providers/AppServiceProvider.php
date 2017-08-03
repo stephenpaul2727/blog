dbca43761f07aad68c207d8dcf0861a267f0c402
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-08-02 22:48:07 -0400

    modified boot service provider for https schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,12 +12,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('REDIRECT_HTTPS'))
-        {
-            $url->forceSchema('https');
-        }
+        //
     }
 
     /**

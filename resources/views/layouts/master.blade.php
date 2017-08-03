56d554773b328b2e9811766d45bd3333c78b0ea3
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-08-02 20:25:39 -0400

    debugging stylesheets

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,8 +10,8 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/master-style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
+    <link rel="stylesheet" href="css/master-style.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>

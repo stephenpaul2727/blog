b72194c35824049294eafd5ad29d4b0529043bac
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-08-02 22:26:53 -0400

    reverting to original link styles

--- resources/views/message/create.blade.php
+++ resources/views/message/create.blade.php
@@ -2,23 +2,25 @@
 
 @section('content')
 <div class="container-fluid bg-3">
-    @if(Session::has('success'))
-        <p class="alert alert-success">{{ Session::get('success') }}</p>
-    @endif
-    <h3 class="margin">Create new message</h3>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    {!! BootForm::open()->action(route('message.store'))->style('font-size:15px;')->post() !!}
-    {!! BootForm::text('E-Mail', 'email') !!}
-    {!! BootForm::textarea('Message','description') !!}
-    {!! BootForm::submit('Submit')->style('display:block;margin:0px auto;') !!}
-    {!! BootForm::close() !!}
+    <div class="container">
+        @if(Session::has('success'))
+            <p class="alert alert-success">{{ Session::get('success') }}</p>
+        @endif
+        <h3 class="margin">Create new message</h3>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        {!! BootForm::open()->action(route('message.store'))->style('font-size:15px;')->post() !!}
+        {!! BootForm::text('E-Mail', 'email') !!}
+        {!! BootForm::textarea('Message','description') !!}
+        {!! BootForm::submit('Submit')->style('display:block;margin:0px auto;') !!}
+        {!! BootForm::close() !!}
+    </div>
 </div>
 @endsection
